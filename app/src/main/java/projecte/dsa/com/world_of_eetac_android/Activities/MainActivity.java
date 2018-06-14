6fe69eeb9ebe88b403ab1d9b45c2759bf8114492
jordicros <jordicros997@gmail.com>
2018-06-14 18:00:16 +0200

    Iniciar partida funcional, amb elecció de mapa. No funciona perque falta definir escenes i transicions al server, pero he comprovat que els Json s'enviessin be i tal.

--- app/src/main/java/projecte/dsa/com/world_of_eetac_android/Activities/MainActivity.java
+++ app/src/main/java/projecte/dsa/com/world_of_eetac_android/Activities/MainActivity.java
@@ -1,5 +1,6 @@
 package projecte.dsa.com.world_of_eetac_android.Activities;
 
+import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
@@ -19,6 +20,7 @@ import java.util.List;
 
 import projecte.dsa.com.world_of_eetac_android.ListAdapters.UsuarisListArrayAdapter;
 import projecte.dsa.com.world_of_eetac_android.Mon.Globals;
+import projecte.dsa.com.world_of_eetac_android.Mon.Partida;
 import projecte.dsa.com.world_of_eetac_android.Mon.Usuario;
 import projecte.dsa.com.world_of_eetac_android.R;
 import projecte.dsa.com.world_of_eetac_android.Services.RetrofitAPI;
@@ -29,7 +31,7 @@ import retrofit2.Response;
 public class MainActivity extends AppCompatActivity {
     List<Usuario> usuaris;
     Globals dades;
-
+    int eleccio;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -44,10 +46,73 @@ public class MainActivity extends AppCompatActivity {
         dades=Globals.getInstance();
 
     }
+    public void startGame(int eleccio) {
+        Partida game = new Partida(Globals.getInstance().getUser().getNickname(),eleccio);
+        RetrofitAPI servei = Globals.getInstance().getServeiRetrofit();
+        Call<Partida> callPartida = servei.startGame(game);
+        callPartida.enqueue(new Callback<Partida>() {
+            int resultat = -1;
 
+            @Override
+            public void onResponse(Call<Partida> call, Response<Partida> resposta) {
+                int codi = resposta.code();
+                Intent intent = new Intent(getApplicationContext(), GameActivity.class);
+                startActivity(intent);
+                //Tot aixo no funciona perque no tenim la partida
+                Log.d("Proba ", "Codi agafat" + codi);
+                if (codi == 200) {
+                    resultat=0;
+                    Partida game = (Partida) resposta.body();
+                    Globals.getInstance().setGame(game);
+                    //INICIAR PARTIDA
+                } else if (codi == 204) {
+                    resultat = -1;
+                }
+                if (resultat == -1) {
+                    AlertDialog.Builder dlgAlert = new AlertDialog.Builder(MainActivity.this);
+                    dlgAlert.setMessage("Error al començar la partida");
+                    dlgAlert.setTitle("Error en les dades");
+                    dlgAlert.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                            //dismiss the dialog
+                        }
+                    });
+                    dlgAlert.setCancelable(true);
+                    dlgAlert.create().show();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<Partida> call, Throwable t) {
+                // log error here since request failed
+                Log.d("Request: ", "error loading API" + t.toString());
+
+            }
+        });
+    }
+    public void eleccio(int i)
+    {
+        this.eleccio = i;
+    }
     public void iniciarGameActivity(View view){
         Intent intent = new Intent(this, GameActivity.class);
-        startActivity(intent);
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Selecció de mapa")
+                .setMessage("Mazmorra o laberint?")
+                .setPositiveButton("Mazmorra", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        eleccio(0);
+                        startGame(eleccio);
+                    }
+                })
+                .setNegativeButton("Laberint", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        eleccio(1);
+                        startGame(eleccio);
+                    }
+                })
+                .setIcon(android.R.drawable.ic_dialog_alert)
+                .show();
     }
     public void usuarisLlista(){
         RetrofitAPI servei = Globals.getInstance().getServeiRetrofit();
